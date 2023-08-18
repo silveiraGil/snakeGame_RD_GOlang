c370ae82baac6e81054f1db1c6fa45d5118b4855
Gilberto <gilberto.silveira@dvcorporate.com>
2023-08-18 19:29:10 +0100

    Add food duration

--- game.go
+++ game.go
@@ -15,7 +15,7 @@ const (
 	screenWidth     = 640
 	screenHeight    = 480
 	foodSpawnPeriod = 3 * time.Second
-	maxFoods        = 3
+	maxFoods        = 4
 	itemSize        = 15
 	Left            = iota
 	Up
@@ -26,9 +26,11 @@ const (
 type game struct{}
 
 type Food struct {
-	X, Y  int
-	Color color.Color
-	name  string
+	X, Y     int
+	Color    color.Color
+	name     string
+	Creation time.Time
+	Duration time.Duration
 }
 
 type SnakeBodyPart struct {
@@ -74,6 +76,10 @@ func createFood() {
 
 	var foodColor color.Color
 	var name string
+	maxSeconds := 15
+	minSeconds := 5
+	randomSeconds := rand.Intn(maxSeconds-minSeconds+1) + minSeconds
+	lifetime := time.Duration(randomSeconds) * time.Second
 
 	switch rand.Intn(4) {
 	case 0:
@@ -91,10 +97,12 @@ func createFood() {
 	}
 
 	food := &Food{
-		X:     x,
-		Y:     y,
-		Color: foodColor,
-		name:  name,
+		X:        x,
+		Y:        y,
+		Color:    foodColor,
+		name:     name,
+		Creation: time.Now(),
+		Duration: lifetime,
 	}
 
 	foods = append(foods, food)
@@ -149,56 +157,16 @@ func drawSnake(screen *ebiten.Image) {
 }
 
 func update() error {
-	// Handle arrow key events to change the snake's direction
-	if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) && direction != Right {
-		direction = Left
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyArrowRight) && direction != Left {
-		direction = Right
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) && direction != Down {
-		direction = Up
-	}
-	if ebiten.IsKeyPressed(ebiten.KeyArrowDown) && direction != Up {
-		direction = Down
-	}
+
+	expireFood()
+	handleArrowKeyEvents()
 
 	// Increment the loop counter
 	loopCount++
-
 	// If the loop count reaches the desired speed, update the snake's position
 	if loopCount >= speed {
-		loopCount = 0 // Reset the loop count
-
-		// Move the body
-		if len(snake.Body) > 0 {
-			// Save the old position of the head
-			oldX, oldY := snakeX, snakeY
-
-			// Update the position of the head based on the direction
-			switch direction {
-			case Left:
-				snakeX -= itemSize
-			case Right:
-				snakeX += itemSize
-			case Up:
-				snakeY -= itemSize
-			case Down:
-				snakeY += itemSize
-			}
-
-			// Move the rest of the body
-			for i := 0; i < len(snake.Body); i++ {
-				// Save the current position of the body part
-				currentX, currentY := snake.Body[i].X, snake.Body[i].Y
-
-				// Move the body part to the previous position of the part in front of it
-				snake.Body[i].X, snake.Body[i].Y = oldX, oldY
-
-				// Update the old position to the current position for the next iteration
-				oldX, oldY = currentX, currentY
-			}
-		}
+		loopCount = 0
+		moveSnake()
 		handleFoodCollisions()
 		handleBodyCollisions()
 		handleEdgeCollisions()
@@ -214,32 +182,7 @@ func handleFoodCollisions() {
 	for i := len(foods) - 1; i >= 0; i-- {
 		food := foods[i]
 		if (snakeX < food.X+itemSize) && (snakeX+itemSize > food.X) && (snakeY < food.Y+itemSize) && (snakeY+itemSize > food.Y) {
-			var score int
-			switch food.name {
-			case "Red":
-				redFruits++
-				score = rand.Intn(5)
-			case "Blue":
-				blueFruits++
-				score = rand.Intn(4)
-			case "White":
-				whiteFruits++
-				score = rand.Intn(3)
-			case "Green":
-				greenFruits++
-				score = rand.Intn(2)
-			}
-
-			totalScore = totalScore + score
-
-			if speed <= 6 {
-				speed = speed + score
-			} else {
-				speed = speed - score
-			}
-
-			updateScore()
-
+			setScore(food.name)
 			foods = append(foods[:i], foods[i+1:]...) // Remove the eaten food
 			addBodyPart()
 		}
@@ -281,6 +224,94 @@ func updateScore() {
 	// Update the game title with the fruit counts and total score
 	title := fmt.Sprintf("Snake Game      |      Red: %d, Green: %d, Blue: %d, White: %d, Score: %d", redFruits, greenFruits, blueFruits, whiteFruits, totalScore)
 	ebiten.SetWindowTitle(title)
+}
+
+func setScore(foodName string) {
+	var score int
+	switch foodName {
+	case "Red":
+		redFruits++
+		score = rand.Intn(5)
+	case "Blue":
+		blueFruits++
+		score = rand.Intn(4)
+	case "White":
+		whiteFruits++
+		score = rand.Intn(3)
+	case "Green":
+		greenFruits++
+		score = rand.Intn(2)
+	}
+
+	totalScore = totalScore + score
+
+	if speed <= 6 {
+		speed = speed + score
+	} else {
+		speed = speed - score
+	}
+
+	updateScore()
+}
+
+func expireFood() {
+	foodsMutex.Lock()
+	defer foodsMutex.Unlock()
+	if len(foods) > 0 {
+		for i := range foods {
+			if time.Now().Sub(foods[i].Creation) > foods[i].Duration {
+				fmt.Printf(foods[i].name + " expired\n")
+				foods = append(foods[:i], foods[i+1:]...)
+				break
+			}
+		}
+	}
+}
+
+func handleArrowKeyEvents() {
+	if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) && direction != Right {
+		direction = Left
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyArrowRight) && direction != Left {
+		direction = Right
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) && direction != Down {
+		direction = Up
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyArrowDown) && direction != Up {
+		direction = Down
+	}
+}
+
+func moveSnake() {
+	if len(snake.Body) > 0 {
+		// Save the old position of the head
+		oldX, oldY := snakeX, snakeY
+
+		// Update the position of the head based on the direction
+		switch direction {
+		case Left:
+			snakeX -= itemSize
+		case Right:
+			snakeX += itemSize
+		case Up:
+			snakeY -= itemSize
+		case Down:
+			snakeY += itemSize
+		}
+
+		// Move the rest of the body
+		for i := 0; i < len(snake.Body); i++ {
+			// Save the current position of the body part
+			currentX, currentY := snake.Body[i].X, snake.Body[i].Y
+
+			// Move the body part to the previous position of the part in front of it
+			snake.Body[i].X, snake.Body[i].Y = oldX, oldY
+
+			// Update the old position to the current position for the next iteration
+			oldX, oldY = currentX, currentY
+		}
+	}
 }
 
 func (g *game) Draw(screen *ebiten.Image) {
