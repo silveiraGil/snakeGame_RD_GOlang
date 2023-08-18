17e311b3ad04fdbb723148240cb852f4c9d796c4
Gilberto <gilberto.silveira@dvcorporate.com>
2023-08-18 16:56:07 +0100

    Create snake initial

--- game.go
+++ game.go
@@ -16,7 +16,7 @@ const (
 	screenHeight    = 480
 	foodSpawnPeriod = 3 * time.Second
 	maxFoods        = 3
-	foodSize        = 15
+	itemSize        = 15
 )
 
 type game struct{}
@@ -26,9 +26,22 @@ type Food struct {
 	Color color.Color
 }
 
+type SnakeBodyPart struct {
+	X, Y  int
+	Color color.Color
+}
+
+type Snake struct {
+	Head     SnakeBodyPart
+	Body     []SnakeBodyPart
+	BodySize int
+}
+
 var (
-	foods      []*Food
-	foodsMutex sync.Mutex // Mutex to protect the foods slice
+	foods          []*Food
+	foodsMutex     sync.Mutex // Mutex to protect the foods slice
+	snake          Snake
+	snakeX, snakeY int
 )
 
 func setScreenColor(screen *ebiten.Image) {
@@ -48,8 +61,8 @@ func createFood() {
 	foodsMutex.Lock()
 	defer foodsMutex.Unlock()
 
-	x := rand.Intn(screenWidth - foodSize)
-	y := rand.Intn(screenHeight - foodSize)
+	x := rand.Intn(screenWidth - itemSize)
+	y := rand.Intn(screenHeight - itemSize)
 
 	var foodColor color.Color
 
@@ -71,18 +84,58 @@ func createFood() {
 	foods = append(foods, food)
 }
 
+func initializeSnake() {
+	// Set snake's head color
+	snake.Head.Color = color.RGBA{77, 77, 77, 255}
+	snake.BodySize = 2
+
+	// Set snake's body colors
+	snake.Body = make([]SnakeBodyPart, snake.BodySize) // Initialize the slice with the correct size
+	for i := range snake.Body {
+		// Set the color for each body part
+		// The body parts closer to the head are lighter in color
+		c := 200 - uint8(i)*20
+		snake.Body[i].Color = color.RGBA{c, c, c, 255}
+	}
+
+	// Set the initial position for the snake's head (center of the screen)
+	snakeX = screenWidth / 2
+	snakeY = screenHeight / 2
+
+	snake.Head.X = snakeX
+	snake.Head.Y = snakeY
+
+	// Set the initial positions for the snake's body parts
+	for i := range snake.Body {
+		snake.Body[i].X = snake.Head.X - (i+1)*itemSize
+		snake.Body[i].Y = snake.Head.Y
+	}
+}
+
 func drawFood(screen *ebiten.Image) {
 	foodsMutex.Lock()
 	defer foodsMutex.Unlock()
 
 	for _, food := range foods {
-		vector.DrawFilledRect(screen, float32(food.X), float32(food.Y), foodSize, foodSize, food.Color, true)
+		vector.DrawFilledRect(screen, float32(food.X), float32(food.Y), itemSize, itemSize, food.Color, true)
+	}
+}
+
+func drawSnake(screen *ebiten.Image) {
+	// Draw snake's head
+	vector.DrawFilledRect(screen, float32(snakeX), float32(snakeY), float32(itemSize), float32(itemSize), snake.Head.Color, true)
+
+	// Draw snake's body
+	for _, part := range snake.Body {
+		bodyColor := color.RGBA{127, 127, 127, 100}
+		vector.DrawFilledRect(screen, float32(part.X), float32(part.Y), float32(itemSize), float32(itemSize), bodyColor, true)
 	}
 }
 
 func (g *game) Draw(screen *ebiten.Image) {
 	setScreenColor(screen)
 	drawFood(screen)
+	drawSnake(screen)
 }
 
 func (g *game) Layout(outsideWidth, outsideHeight int) (int, int) {
@@ -101,6 +154,9 @@ func main() {
 
 	// Start the goroutine to create food
 	go createFoodRoutine()
+
+	// Initialize the snake
+	initializeSnake()
 
 	// Start the game
 	if err := ebiten.RunGame(&game{}); err != nil {
